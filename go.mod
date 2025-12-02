843acf37a0edcd8aed2e2dafb7ce4162104f8ebc
wai-lin <dev.wailin@gmail.com>
2025-12-02 09:51:26 +0700

    batman

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hs.com/devops
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Simple struct {
+	Name        string
+	Description string
+	Url         string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	simple := Simple{"Hello", "World", r.Host}
+
+	jsonOutput, _ := json.Marshal(simple)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Fprintln(w, string(jsonOutput))
+}
+
+func main() {
+	fmt.Println("Server started on port 4444")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":4444", nil))
+}
