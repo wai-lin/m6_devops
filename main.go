c3a8e577e23278a7dd2a65751db88c09d39c8f6f
Wai Lin <dev.wailin@gmail.com>
2025-12-05 10:20:05 +0700

    Change SimpleFactory to use 'Ansible' instead of 'Jenkins'

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-    return Simple{"Hello", "Jenkins", host}
+    return Simple{"Hello", "Ansible", host}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
