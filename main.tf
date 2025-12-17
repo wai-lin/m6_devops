2ce2f4ef7702217b6250ce9879376a839827c063
Wai Lin <dev.wailin@gmail.com>
2025-12-17 19:24:48 +0700

    Change AWS key pair name and clear public key
    Updated the AWS key pair configuration to use a new key name and cleared the public key value.

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_key_pair" "main" {
+  key_name   = "mykey"
+  public_key = ""
+}
+
+resource "aws_security_group" "main" {
+  name        = "allow-ssh-and-4444"
+  description = "Allow SSH and port 4444 from anywhere"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 4444 from anywhere"
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh-and-4444"
+  }
+}
+
+resource "aws_instance" "main" {
+  ami           = "ami-00d8fc944fb171e29"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.main.id
+  
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
