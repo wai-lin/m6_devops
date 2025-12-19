0486c0fdfc854af84c47763f33d5ad5f69f839f3
wai-lin <dev.wailin@gmail.com>
2025-12-19 09:15:23 +0700

    create node project with Dockerfile

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 4444
+
+function Sample(host) {
+	return { name: "Hello", description: "World", url: host }
+}
+
+app.get('/', (req, res) => {
+	res.json(Sample(req.headers.host))
+})
+
+var server = app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
+
+process.on('SIGINT', () => {
+	console.log('Stopping ...');
+	server.close(() => {
+		console.log('Stopped');
+	});
+});
+
+module.exports = {
+	Sample,
+	server
+};
